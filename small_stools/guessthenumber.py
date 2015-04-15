13eeef63fb026f9e28330a7b3b41ee1dd3f66777
huhu8 <zhuhuhu118@gmail.com>
2015-04-15 16:08:34 +0800

    second stool

--- small_stools/guessthenumber.py
+++ small_stools/guessthenumber.py
@@ -8,38 +8,54 @@ import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 # initialize global variables
 hint = "Welcome"
+global secretnumber100
+global secretnumber1000
 
 # user select a game range and start the game
 def select1():
     global secretnumber100 #忘了为什么要把这个设置成全局变量了，是为了方便其他函数调用吗？
     secretnumber100 = random.randrange(1,100)
-    #return secretnumber100 # 这个语句是不是可以去掉，secretnumber已经产生是不是就已经存储了 问题有可能错误
+    return secretnumber100 # 这个语句是不是可以去掉，secretnumber已经产生是不是就已经存储了 问题有可能错误
 
 def select2():
     global secretnumber1000 
     secretnumber1000 = random.randrange(1,1000)
-    #return secretnumber1000 #
+    return secretnumber1000 #这个语句不能去掉，下面的函数guessthenumber需要调用这个数值
 
 def guessnumber(inp):
-    guessnumber = int(inp)
-    global hint 
-    if guessnumber > secretnumber100:
-       hint = "lower"
-    elif guessnumber < secretnumber100:
-         hint = "higher"
-    elif guessnumber == secretnumber100:
-         hint = "right"
-    return hint #如果去掉return呢
+    if select1():
+        guessnumber = int(inp)
+        global hint
+#        global secretnumber100
+        if guessnumber > secretnumber100:
+           hint = "lower"
+        elif guessnumber < secretnumber100:
+             hint = "higher"
+        elif guessnumber == secretnumber100:
+             hint = "right"
+             return hint
+    elif select2():
+        guessnumber = int(inp)
+        global hint
+#        global secretnumber1000
+        if guessnumber > secretnumber1000:
+           hint = "lower"
+        elif guessnumber < secretnumber1000:
+             hint = "higher"
+        elif guessnumber == secretnumber1000:
+             hint = "right"
+        return hint
 
+      
 # draw the result in canvas  
 def draw(canvas):
     canvas.draw_text(hint,[80,150],50,"Red")
     
 #0414感觉这后面的语句没有必要，注释掉看看情况
 #if select1():
-#  canvas.draw_text(hint,[80,150],50，"Red")
+#   canvas.draw_text(hint,[80,150],50,"Red")
 #elif select2():
-#   canvas.draw_text(hint,[60,150],50，"Red")
+#     canvas.draw_text(hint,[80,150],50,"Red")
 #draw the results in canvas
 
 # create frame
